e59dd35030d1e00c152316ce98e36192079c6a48
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-24 19:37:52 +0200

    CardGame UI design created, assets added

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -9,8 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Image("logo").resizable().aspectRatio(contentMode: .fit)
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: 170, height: 150)
+            Text("WAR GAME").font(.headline)
+            
+            Spacer()
+            HStack{
+                Spacer()
+                Image("card2")
+                Spacer()
+                Image("card12")
+                Spacer()
+            }
+            Spacer()
+            Button {
+                
+            } label: {
+                Image("dealbutton").padding()
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                VStack{
+                    Text("Player").font(.title)
+                        .padding(.bottom,10)
+                    Text("0").font(.largeTitle)
+
+                }
+                Spacer()
+                VStack{
+                    Text("Computer").font(.title)
+                        .padding(.bottom,10)
+                    Text("0").font(.largeTitle)
+                }
+                Spacer()
+            }
+        }
+        .background(
+                Image("background")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+            )
+        .foregroundColor(Color.white)
     }
 }
 
