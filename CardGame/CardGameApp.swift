edc5263b5759b1e87bcfb6c73572aedf147344ec
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-24 18:34:21 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Linda Imeri on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
